c836dab734614fd914f25cd65ed8886e8f62eed0
Shagun <shagunchahar8@gmail.com>
2023-03-02 01:02:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quiz/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.example.quiz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this,SignUp::class.java)
+            startActivity(intent)
+            finish()
+        },1500)
+    }
+}
