103b67314b32710eaee165b826b21c2e058f3039
Shagun <shagunchahar8@gmail.com>
2023-03-02 10:49:52 +0530

    code page start

--- app/src/main/java/com/example/quiz/Login.kt
+++ app/src/main/java/com/example/quiz/Login.kt
@@ -1,11 +1,53 @@
 package com.example.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Toast
+import com.example.quiz.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 
 class Login : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+    private lateinit var database : DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.Login.setOnClickListener {
+
+            val email = binding.email.text.toString()
+            val passcode = binding.passcode.text.toString()
+
+            if (email.isNotBlank()&&passcode.isNotBlank()){
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email,passcode).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        val intent = Intent(this,CODE::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                    else{
+                        Toast.makeText(this, "Signin Failed" , Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+
+
+
+        }
+        binding.signup.setOnClickListener {
+            val intent = Intent(this,SignUp::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/quiz/SignUp.kt
+++ app/src/main/java/com/example/quiz/SignUp.kt
@@ -1,11 +1,56 @@
 package com.example.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.example.quiz.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 
 class SignUp : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+    private lateinit var database : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        val binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.Signup.setOnClickListener {
+
+            val email = binding.email.text.toString()
+            val passcode = binding.passcode.text.toString()
+            val rollno = binding.rollno.text.toString()
+            if (Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                if (passcode.isNotEmpty()&&rollno.isNotEmpty()) {
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, passcode).addOnCompleteListener {
+                        val intent = Intent(this , CODE::class.java)
+                        startActivity(intent)
+                        finish()
+
+                    }.addOnFailureListener {
+                        Toast.makeText(this,"Sign up Failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                else{
+                    Toast.makeText(this,"Enter All details", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else{
+                Toast.makeText(this,"Enter Correct Mail", Toast.LENGTH_SHORT).show()
+            }
+
+
+
+        }
+        binding.login.setOnClickListener {
+            val intent = Intent(this,Login::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
     }
 }
