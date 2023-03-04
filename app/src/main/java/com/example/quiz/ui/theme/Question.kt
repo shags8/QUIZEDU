417c921ee1297bac789552c9a883f90046b02691
Shagun <shagunchahar8@gmail.com>
2023-03-05 00:02:56 +0530

    code page start

---
+++ app/src/main/java/com/example/quiz/ui/theme/Question.kt
@@ -0,0 +1,12 @@
+package com.example.quiz.ui.theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.quiz.R
+
+class Question : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+    }
+}
